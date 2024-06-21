77517e2bbe8721c31edcea85744a75d4a3481bd7
laurennelsen <77991089+laurennelsen@users.noreply.github.com>
2024-06-21 17:43:30 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,11 +10,56 @@ var ptx_lunr_docs = [
   "body": "  My Website   copyright  "
 },
 {
+  "id": "sec-instructor-info",
+  "level": "1",
+  "url": "sec-instructor-info.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Instructor Information",
+  "body": " Instructor Information  Welcome to QUAN 2010!   Instructor: Dr. Lauren M. Nelsen   Email: lnelsen@uccs.edu (Note that Nelsen is spelled “en” instead of “on” at the end.)   Office Location: Dwire Hall #362   Office Hours:     In-person office hours: TBA     Online office house: TBA (Go to the Zoom tab on Canvas.)     Course Location: Dwire Hall Room 122      Course Communication  I do my best to reply to emails within 24 hours, but on weekends it may take longer.  I will use both Canvas and email to communicate with the class.   "
+},
+{
+  "id": "sec-opportunities-for-help",
+  "level": "1",
+  "url": "sec-opportunities-for-help.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Opportunities for Help",
+  "body": " Opportunities for Help   Office Hours  I encourage you to come to my office hours! They are a good opportunity to ask questions, get additional explanations or help with course work!  Look at to see times for in-person and online office hours.    Tutoring  Free tutoring is available both in Dwire and in the Excel Math Center.     QUAN Tutoring in Dwire: ...insert link to schedule....    Excel Math Center:       "
+},
+{
+  "id": "sec-course-content-format",
+  "level": "1",
+  "url": "sec-course-content-format.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Course Content and Format",
+  "body": " Course Content and Format   Course Description  Statistical applications in business. Includes descriptive statistics, probability distributions, sampling theory, estimation, hypothesis testing, and simple and multiple regression.   Prerequisite Information:   Prer., INFS 1100; MATH 1040, Sophomore standing or MATH 1120, Business students only    Course Format  The course will meet in person in Dwire Hall 122 (A tentative schedule is included later in the syllabus.) On Canvas, the course is organized by weekly modules.  I will provide you with blank notes, Excel templates that go along with the notes, and homework problems to work in MyLab Statistics. Here is the routine I recommend you follow to get the most out of this course:   Read the assigned sections of the book from the attached schedule and fill in the definitions on the blank notes.    Come to class meetings and work the example problems on the notes alongside me.    Complete your MyLab Statistics homework assignments.    Look at quiz objectives before each quiz.       Course Learning Objectives  Upon successful completion of this course:     Students will demonstrate the appropriate graphical and numerical descriptive statistics to summarize different types of data.    Students will apply basic probability and probability distribution concepts to business scenarios.    Students will utilize tools of inferential statistics to estimate population parameters and test claims made about population parameters within business contexts.    Students will build and assess regression models to analyze relationships between variables.    Students will perform data analysis by selecting the appropriate quantitative tool (Microsoft Excel).      "
+},
+{
+  "id": "sec-required-materials",
+  "level": "1",
+  "url": "sec-required-materials.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Required Materials",
+  "body": " Required Materials   Required Texts\/Readings\/Materials     Business Statistics, 3rd edition, by Robert A. Donnelly, Jr. with MyLab Statistics subscription. (ISBN 9780136421443). You can access this book through Vitalscource in Canvas.    Microsoft Excel with the Data Analysis Add-In enabled.       Technology Requirements  This course requires the use of a UCCS email account, a computer\/laptop, and a reliable internet connection. Review the UCCS Online Technical Requirements for exact specifications. Communication will be by UCCS email and Canvas. If your UCCS email is not your primary address, consider forwarding your UCCS email address to another account you check often. All official course information must be sent through your UCCS account or Canvas.  (We will be meeting in a computer lab, so it's okay if you don't have a personal computer -- you can use a computer in our classroom. But you are still expected to check email\/Canvas regularly.)    Technology Competencies  You are expected to begin this course with basic computing skills that include using Canvas, Microsoft Word, accessing online research databases, and corresponding using your UCCS email. Knowledge of certain multimedia tools, such as PowerPoint or other audio\/video platforms is a plus. See UCCS Online Technical Skills for more information.   "
+},
+{
+  "id": "sec-assignments-and-grading",
+  "level": "1",
+  "url": "sec-assignments-and-grading.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Assignments and Grading",
+  "body": " Assignments and Grading   Assignments   Grading Category Weights:      Quiz Objectives     MyLab Homework (Conceptual)     MyLab Homework (Problems)     Exam 1     Exam 2     Final Exam     Total:       Types of Assignments:     MyLab Homework:    Homework problems will require your understanding of statistical concepts and their application to problem contexts. You will complete these each week in MyLab Statistics. The homework assignments will typically be due each week at 11:59pm the weeknight before our class meeting. (However, I strongly recommend trying to complete the homework before then.) You should get multiple attempts for MyLab homework problems.    I will drop the lowest conceptual score from each of the MyLab Homework categories.        Quizzes:    There will be several quizzes throughout the course. Each of quizzes will be made up of several objectives. You need to do the problem(s) corresponding to each objective perfectly to earn full credit for that objective.    On each of the days specified in the schedule, you will be allowed to re-try certain quizzes\/objectives. This will give you another chance to earn credit for objectives that you missed on your first attempt.    For quizzes you may use a 4x6 inch notecard, a basic, nonprogrammable calculator (no phone) and Excel (if the problem allows it).    During quizzes you may NOT search the internet or use other resources that are not allowed.        Exams:    There will be 3 in-class exams during this course: 2 midterm exams and a final exam. Questions will require your understanding of statistical concepts and their application to problem contexts.    You will be allowed to use an 8.5x11 inch sheet of notes and a basic, nonprogrammable calculator (no phone) during the exams. You must show your work and answers must be clearly presented to receive full credit. (Some problems may also allow the use of Excel.)    During exams you may NOT search the internet or use other resources that are not allowed.          Missed\/Late Work  I do not have to allow make-ups or accept late work, but I often consider them according to each student's situation. I strongly recommend asking me for extensions if you find yourself needing them. In general, communicating with me earlier rather than later about your situation makes it more likely that we can make some kind of arrangement appropriate to your situation. Let me know as soon as possible if you are going to miss a quiz or exam. If you don't notify me in advance of your absence, or if you do not have a valid excuse, you may not be allowed to make up the assignment\/assessment, and you may receive a score of zero for that assignment\/assessment.    How to be successful in QUAN 2010     Come to class and pay attention\/be engaged as we're working in class.    Ask for help as soon as you realize you need it!    Come to office hours!    Commit to spending a minimum of 5 hours per week (split among at least 3 different days) to complete and submit the assignments for the course on time.    Form a study group with your classmates and meet regularly to practice problems.    Check UCCS email daily for updates or Canvas announcements regarding the course, and reply to those messages when appropriate.    Regularly review the syllabus\/calendar so you don't miss due dates.    Read\/study the book!    Go to the QUAN tutoring center in Dwire (...insert link to schedule...) and\/or the Mathematics Excel Center for tutoring https:\/\/mathcenter.uccs.edu       "
+},
+{
   "id": "sec-summary-of-objectives",
   "level": "1",
   "url": "sec-summary-of-objectives.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "2.1",
   "title": "QUAN 2010 Quiz Objectives",
   "body": " QUAN 2010 Quiz Objectives  (Note: These are subject to changes.)      Quiz 1       1a: Types of variables   I can classify a variable as Quantitative or Qualitative, as well as the subcategory Continuous, Discrete, Nominal or Ordinal.     1b: Visualizing Data   Given a sample of data, I can generate visualizations to represent its variables.        Quiz 2       2a: Centers of Data   Given a sample of data, I can identify or compute different measures of centrality.     2b: Variation of Data   Given a sample of data, I can identify or compute different measures of variation, identify outliers, and construct a box-and-whisker plot for the data.        Quiz 3       3a: Probability   I can compute probabilities of events, including compound events involving operations and probability of events involving conditional probabilities.     3b: Random Variables   I can compute probabilities from the probability distribution of a random variable, as well as compute the expectation, variance and standard deviation of a random variable.        Quiz 4       4a: Standard Normal Variables   I can compute probabilities given bounds, and bounds given probabilities for the standard normal variable.     4b: General Normal Distribution   I can compute probabilities given bounds, and bounds given probabilities for general normal variables.     4c: Binomial Distribution   I can compute probabilities for binomial random variables.        Quiz 5       5a: Confidence Intervals   I can find a confidence interval for the true proportion of a categorical variable, given a sample, and interpret the meaning of this interval.     5b: Size of Sample: Proportion   I can identify the sample size necessary for a confidence interval to have a given margin of error.        Quiz 6       6a: Hypothesis Testing   I can test a hypothesis about the true proportion of a categorical variable, given a sample: stating the null and alternative hypothesis, computing a p-value, explaining the meaning of the p-value and drawing a conclusion.     6b: Inference for Single Numerical Variables   I can perform hypothesis tests and compute confidence intervals for the means of numerical variables and explain the results.        Quiz 7       7a: Inference for Difference of Means of Numerical Variables   I can perform hypothesis tests and compute confidence intervals for the differences of means of numerical variables, and explain the results.     7b: Inference for paired Numerical Variables   I can perform hypothesis tests and compute confidence intervals for the means of differences of paired numerical variables, and explain the results.        "
 },
@@ -23,7 +68,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-1.html",
   "type": "Section",
-  "number": "1.2",
+  "number": "2.2",
   "title": "Quiz 1 Example Problems",
   "body": " Quiz 1 Example Problems  Below are example problems for Quiz 1.      Solutions     Excel files and\/or written work for solutions:        Videos of Solutions:    Objective 1a:         Objective 1b:             "
 },
@@ -32,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-2.html",
   "type": "Section",
-  "number": "1.3",
+  "number": "2.3",
   "title": "Quiz 2 Example Problems",
   "body": " Quiz 2 Example Problems  Below are example problems for Quiz 2.      Solutions     Excel files and\/or written work for solutions:         Videos of Solutions:    Objective 2a:         Objective 2b:             "
 },
@@ -41,7 +86,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-3.html",
   "type": "Section",
-  "number": "1.4",
+  "number": "2.4",
   "title": "Quiz 3 Example Problems",
   "body": " Quiz 3 Example Problems  Below are example problems for Quiz 3.      Solutions     Excel files and\/or written work for solutions:         Videos of Solutions:    Objective 3a:         Objective 3b:             "
 },
@@ -50,7 +95,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-4.html",
   "type": "Section",
-  "number": "1.5",
+  "number": "2.5",
   "title": "Quiz 4 Example Problems",
   "body": " Quiz 4 Example Problems  Below are example problems for Quiz 4.      Solutions     Excel files and\/or written work for solutions:        Videos of Solutions:    Objective 4a:         Objective 4b:             "
 },
@@ -59,7 +104,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-5.html",
   "type": "Section",
-  "number": "1.6",
+  "number": "2.6",
   "title": "Quiz 5 Example Problems",
   "body": " Quiz 5 Example Problems  Below are example problems for Quiz 5.      Solutions     Excel files and\/or written work for solutions:        Videos of Solutions:    Objective 5a:         Objective 5b:             "
 },
@@ -68,7 +113,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-6.html",
   "type": "Section",
-  "number": "1.7",
+  "number": "2.7",
   "title": "Quiz 6 Example Problems",
   "body": " Quiz 6 Example Problems  Below are example problems for Quiz 6.      Solutions     Excel files and\/or written work for solutions:        Videos of Solutions:    Objective 6a:         Objective 6b:             "
 },
@@ -77,7 +122,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-quiz-7.html",
   "type": "Section",
-  "number": "1.8",
+  "number": "2.8",
   "title": "Quiz 7 Example Problems",
   "body": " Quiz 7 Example Problems  Below are example problems for Quiz 7.      Solutions     Excel files and\/or written work for solutions:         Videos of Solutions:    Objective 7a:         Objective 7b:             "
 },
